c537f32cb1f8506b579d099e1db7a8bd4c61889b
66RING <1552087953@qq.com>
2020-04-21 20:47:07 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -4,7 +4,6 @@
 #/bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
 picom --config ~/.config/picom.conf -b
-#picom -o 0.95 -i 0.8 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 wmname LG3D
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &

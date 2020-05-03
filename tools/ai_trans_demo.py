b1b96aee69b06ca55fdf822982b2d2b33b8bf5b9
66RING <1552087953@qq.com>
2020-05-03 20:01:47 +0800

    trans tool

---
+++ tools/ai_trans_demo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import requests
+import sys
+import json
+
+
+def parse_page(res):
+    data = json.loads(res)
+    return data
+
+
+def get_page_index(Str):
+    URL= "https://aidemo.youdao.com/trans"
+    Strend = ''
+    data = {
+        "q":Str,
+        "from":"AUTO",    
+        "to":"AUTO",
+    }
+
+    res = requests.post(url=URL, data=data)
+    res = res.content.decode("utf-8")
+    return res
+
+
+def main():
+
+    Str = ' '.join(sys.argv[1:])
+
+    if Str != '':
+        res = get_page_index(Str)
+        items = parse_page(res)
+
+        print(items.get('translation')[0])
+        if items.get('web'):
+            print('\n=== dict ===')
+            for item in items.get('web'):
+                print(', '.join(item.get('value')))
+
+
+if __name__ == "__main__":
+    main()

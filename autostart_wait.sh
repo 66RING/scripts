52787a8b53fd067331296608245f9d118227fa29
66RING <1552087953@qq.com>
2020-05-25 11:10:26 +0800

    update

--- autostart_wait.sh
+++ autostart_wait.sh
@@ -1,6 +1,5 @@
 
-
-sleep 5
+sleep 1
 xss-lock slock &
 fcitx &
 #goldendict &
--- wp-change.sh
+++ wp-change.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-feh --recursive --randomize --bg-fill --no-fehbg ~/Pictures/wallpapers/macOS
+feh --recursive --randomize --bg-fill --no-fehbg ~/Pictures/cw_wallpaper

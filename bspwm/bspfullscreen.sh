403f205292355950a18be1f8f043072e893e737c
66RING <1552087953@qq.com>
2020-07-15 22:30:26 +0800

    bspwm toggle

--- bspwm/bspfullscreen.sh
+++ bspwm/bspfullscreen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if [ -z "$(bspc query -N -n .focused.fullscreen -d focused)" ]; then
-    #bspc node focused.tiled -t fullscreen
-    bspc node focused -t fullscreen
-else
-    #bspc node focused.fullscreen -t tiled 
-    bspc node focused -t \~fullscreen 
-fi
+#if [ -z "$(bspc query -N -n .focused.fullscreen -d focused)" ]; then
+#    #bspc node focused.tiled -t fullscreen
+#    bspc node focused -t fullscreen
+#else
+#    #bspc node focused.fullscreen -t tiled 
+#    bspc node focused -t \~fullscreen 
+# fi
+bspc node focused -t \~fullscreen 
 
--- bspwm/bsphideandfullscreen.sh
+++ bspwm/bsphideandfullscreen.sh
@@ -2,8 +2,8 @@
 
 if [ -z "$(bspc query -N -n .focused.fullscreen -d focused)" ]; then
     bspc node focused -t fullscreen
-    /home/ring/scripts/bspwm/bsphide.sh
+    /home/ring/scripts/bspwm/bsphideall.sh
 else
-    bspc node focused.fullscreen -t \~fullscreen
+    bspc node focused -t \~fullscreen
     /home/ring/scripts/bspwm/bspunhideall.sh
 fi

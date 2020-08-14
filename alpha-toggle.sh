0f796b48971dec37e7c4b79f548b723ca1ff9b1b
66RING <1552087953@qq.com>
2020-08-14 14:22:56 +0800

    auto alpha and border

--- alpha-toggle.sh
+++ alpha-toggle.sh
@@ -4,8 +4,10 @@ result=$(ps ax|grep -v grep|grep picom)
 
 if [ "$result" == "" ]; then
   eval "picom -b --experimental-backends"
+  bspc config border_width         0
   notify-send "compositor on" &
 else
   eval "killall picom"
+  bspc config border_width         5
   notify-send "compositor off" &
 fi
